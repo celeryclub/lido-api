c4c866d7bed9176e923daff6bd951356ec69263d
Steve Davis <steve@celery.club>
2017-11-20 23:43:05 -0500

    Add CORS headers

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+
+  next()
+})
+
 app.use('/', index)
 app.use('/notes', notes)
 

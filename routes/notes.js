b167451a466a1d10b831c7321a0555141877aef1
Steve Davis <steve@celery.club>
2017-11-20 23:13:54 -0500

    Add ID to note test data

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const createNote = (name) => {
+const createNote = (id, name) => {
   return {
+    id: id,
     name: name,
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    body: '# Lorem\r\n\r\n* ipsum\r\n* dolor\r\n\r\nsit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     createdAt: Date.now(),
     updatedAt: Date.now(),
     publishedAt: Date.now(),
@@ -14,10 +15,10 @@ const createNote = (name) => {
 router.get('/', function(req, res, next) {
   res.json({
     notes: [
-      createNote('Hello'),
-      createNote('Test'),
-      createNote('Jim\'s note'),
-      createNote('Welcome'),
+      createNote(1, 'Hello'),
+      createNote(2, 'Test'),
+      createNote(3, 'Jim\'s note'),
+      createNote(4, 'Welcome'),
     ]
   })
 })
